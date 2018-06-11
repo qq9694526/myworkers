73ba6c516170afdfaeb70cc2d2b6f5cebf57b2a8
zhaohaipeng <9694526@qq.com>
2018-06-12 00:03:27 +0800

    data.js修改img为相对路径

--- static/data.js
+++ static/data.js
@@ -4,8 +4,8 @@ export var DATA=[
 		"style":"单页应用",
 		"skills":"Vue、Vue-router、betterScroll",
 		"funs":"商家、菜品、评价、小球动画、购物车、滚动区域联动等",
-		"img":"/static/img/01.png",
-		"btn":"/static/img/1.png",
+		"img":"../img/01.png",
+		"btn":"/src/assets/img/img/1.png",
 		"url":"http://www.zhaohaipeng.com/sell"
 	},
 	{
@@ -13,8 +13,8 @@ export var DATA=[
 		"style":"微信端、混合开发",
 		"skills":"Mui、Ajax、Promise",
 		"funs":"酒店、酒品、订单、收藏、个人中心、地图定位、支付、第三方登录、H5活动等",
-		"img":"/static/img/02.png",
-		"btn":"/static/img/2.png",
+		"img":"http://www.zhaohaipeng.com/img/02.png",
+		"btn":"http://www.zhaohaipeng.com/img/2.png",
 		"url":"http://wx.jiujiumiandan.cn/index.aspx"
 	},
 	{
@@ -22,8 +22,8 @@ export var DATA=[
 		"style":"响应式企业站",
 		"skills":"jQuery、BootStrap、Swiper",
 		"funs":"过渡动画、轮播组件、在线客服、在线报名、地图找驾校等",
-		"img":"/static/img/03.png",
-		"btn":"/static/img/3.png",
+		"img":"http://www.zhaohaipeng.com/img/03.png",
+		"btn":"http://www.zhaohaipeng.com/img/3.png",
 		"url":"http://www.guolaxueche.com/"
 		
 	},
@@ -32,8 +32,8 @@ export var DATA=[
 		"style":"全屏+自适应",
 		"skills":"FullPage",
 		"funs":"全屏轮播、过渡动画",
-		"img":"/static/img/04.gif",
-		"btn":"/static/img/4.png",
+		"img":"http://www.zhaohaipeng.com/img/04.gif",
+		"btn":"http://www.zhaohaipeng.com/img/4.png",
 		"url":"http://jiujiumiandan.com/"
 		
 	},
@@ -41,8 +41,8 @@ export var DATA=[
 		"name":"大转盘",
 		"style":"H5活动",
 		"skills":"Css3",
-		"img":"/static/img/05.png",
-		"btn":"/static/img/5.png",
+		"img":"http://www.zhaohaipeng.com/img/05.png",
+		"btn":"http://www.zhaohaipeng.com/img/5.png",
 		"url":"http://zhaohaipeng.com/DEMO/Works/turnTable/index.html"
 		
 	},
@@ -50,8 +50,8 @@ export var DATA=[
 		"name":"刮刮乐",
 		"style":"H5活动",
 		"skills":"Canvas",
-		"img":"/static/img/06.png",
-		"btn":"/static/img/6.png",
+		"img":"http://www.zhaohaipeng.com/img/06.png",
+		"btn":"http://www.zhaohaipeng.com/img/6.png",
 		"url":"http://zhaohaipeng.com/DEMO/Works/scratchCardByCanvas/index.html"
 	}
 ]
